8f0dfac726a1287893fe66cb8f7d5ea0a97dcb04
garedchapman <c15gared.chapman@usafa.edu>
2013-10-23 23:31:23 -0600

    API and given implement with added INITSPI

---
+++ LCD_implement.c
@@ -0,0 +1,113 @@
+/*
+ * LCD_implement.c
+ *
+ *  Created on: Oct 22, 2013
+ *      Author: C15Gared.Chapman
+ */
+
+void INITSPI(void)
+{
+	UCB0CTL1 |= UCSWRST;
+	UCB0CTL0 |= UCCKPH|UCMSB|UCMST|UCSYNC;
+	UCB0CTL1 |= UCSSEL1;
+	UCB0STAT |= UCLISTEN;
+	P1SEL |= BIT5;
+	P1SEL2 |= BIT5;
+	P1SEL |= BIT6;
+	P1SEL2 |= BIT6;
+	P1SEL |= BIT7;
+	P1SEL2 |= BIT7;
+
+	UCB0CTL1 &= UCSWRST;
+}
+
+
+#define RS_MASK 0x40
+
+void LCDinit()
+{
+    writeCommandNibble(0x03);
+
+    writeCommandNibble(0x03);
+
+    writeCommandNibble(0x03);
+
+    writeCommandNibble(0x02);
+
+    writeCommandByte(0x28);
+
+    writeCommandByte(0x0C);
+
+    writeCommandByte(0x01);
+
+    writeCommandByte(0x06);
+
+    writeCommandByte(0x01);
+
+    writeCommandByte(0x02);
+
+    SPI_send(0);
+    delayMicro();
+}
+
+
+void LCDclear()
+{
+    writeCommandByte(1);
+}
+
+void writeCommandNibble(char commandNibble)
+{
+    LCDCON &= ~RS_MASK;
+    LCD_write_4(commandNibble);
+    delayMilli();
+}
+
+void writeCommandByte(char commandByte)
+{
+    LCDCON &= ~RS_MASK;
+    LCD_write_8(commandByte);
+    delayMilli();
+}
+
+void writeDataByte(char dataByte)
+{
+    LCDCON |= RS_MASK;
+    LCD_write_8(dataByte);
+    delayMilli();
+}
+
+void LCD_write_8(char byteToSend)
+{
+    unsigned char sendByte = byteToSend;
+
+    sendByte &= 0xF0;
+
+    sendByte = sendByte >> 4;               // rotate to the right 4 times
+
+    LCD_write_4(sendByte);
+
+    sendByte = byteToSend;
+
+    sendByte &= 0x0F;
+
+    LCD_write_4(sendByte);
+}
+
+void SPI_send(char byteToSend)
+{
+    volatile char readByte;
+
+    set_SS_lo();
+
+    UCB0TXBUF = byteToSend;
+
+    while(!(UCB0RXIFG & IFG2))
+    {
+        // wait until you've received a byte
+    }
+
+    readByte = UCB0RXBUF;
+
+    set_SS_hi();
+}
